d1b87db4ddc5d8cfd747932147ce19e284ea6f1a
rbeskrovnyi <rbeskrovnyi@sciforce.solutions>
2017-09-29 15:55:36 +0300

    added tests for a application context and for welcome controller.

--- src/main/java/ratings/student/controllers/WelcomeController.java
+++ src/main/java/ratings/student/controllers/WelcomeController.java
@@ -4,9 +4,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Map;
 
+/**
+ * {@link Controller} for a welcome page.
+ */
 @Controller
 public class WelcomeController {
 
@@ -16,7 +20,7 @@ public class WelcomeController {
     private String message = "Hello World";
 
     @RequestMapping( value = "/", method = RequestMethod.GET )
-    public String welcomePage( Map<String, Object> model ) {
+    public @ResponseBody String welcomePage(Map<String, Object> model ) {
         model.put( "page", WELCOME_PAGE );
         model.put("message", this.message);
         model.put( "method", "GET" );
---
+++ src/test/java/ratings/student/controllers/WelcomeControllerTest.java
@@ -0,0 +1,24 @@
+package ratings.student.controllers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(WelcomeController.class)
+public class WelcomeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testWelcomePage() throws Exception {
+        mockMvc.perform( get("/") ).andExpect( status().isOk() );
+    }
+}
