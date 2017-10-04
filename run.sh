9b6b75207e4d17c709b2f9d186f855bcd299c15d
romankh3 <romankh3@bitbucket.org>
2017-10-04 20:58:36 +0300

    fixed problems with thymeleaf.

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+#
+# Executable file to run an application.
+#
+./gradlew clean bootRun
--- src/main/java/ratings/student/controllers/WelcomeController.java
+++ src/main/java/ratings/student/controllers/WelcomeController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Map;
 
@@ -14,16 +13,14 @@ import java.util.Map;
 @Controller
 public class WelcomeController {
 
-    private static final String WELCOME_PAGE = "welcome";
-
     @Value("${welcome.message:test}")
     private String message = "Hello World";
 
     @RequestMapping( value = "/", method = RequestMethod.GET )
-    public @ResponseBody String welcomePage(Map<String, Object> model ) {
-        model.put( "page", WELCOME_PAGE );
+    public String welcomePage( Map<String, Object> model ) {
+        model.put( "page", "welcomePage, Man." );
         model.put("message", this.message);
         model.put( "method", "GET" );
-        return WELCOME_PAGE;
+        return "welcome";
     }
 }
